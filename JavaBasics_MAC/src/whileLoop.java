a6904d455a929e6d903304d0bde6148b489f9f1c
Ashu <Ashu@localhost>
2019-10-21 15:46:31 -0700

    While loop added

---
+++ JavaBasics_MAC/src/whileLoop.java
@@ -0,0 +1,20 @@
+
+public class whileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int value=0;
+		
+		while(value<10)
+		{
+			System.out.println("Number"+value);
+			value=value+2;
+		}
+		
+
+				
+
+	}
+
+}
