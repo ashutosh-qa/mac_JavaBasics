2e66a6463ed8632848d8cfc41d48d0bfc019bef9
Ashu <Ashu@localhost>
2019-10-21 13:27:50 -0700

    Class name updated, added variables

---
+++ JavaBasics_MAC/src/Variables.java
@@ -0,0 +1,43 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World, I am ready!!!");
+		
+		int myNumber=27;
+		short myShort=847;
+		long myLong=4354354;
+		
+		double myDouble=7.3232;
+		float myFloat=232.43f;
+		
+		char myChar='a';
+		boolean myBoolean=true;
+		
+		byte myByte=127;
+		
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		
+		System.out.println(myByte);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
