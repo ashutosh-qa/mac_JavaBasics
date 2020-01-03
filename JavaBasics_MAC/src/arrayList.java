2fdfc3ce6d91d318f596bed39f5a58deee4e18bb
Ashu <Ashu@localhost>
2020-01-03 12:38:12 -0800

    Array List

---
+++ JavaBasics_MAC/src/arrayList.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class arrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+
+			ArrayList<String> al=new ArrayList<String>();
+			
+			al.add("Ashutosh Singh is the new Java Programmer");
+			al.add("No1");
+			//System.out.println(al);
+			//al.add(0, "Hello");
+			//System.out.println(al);
+			
+			
+			System.out.println(al.contains("No1"));
+			System.out.println(al.indexOf("Ashutosh Singh is the new Java Programmer"));
+			System.out.println(al.size());
+
+			
+			
+		}
+
+	}
+
+
